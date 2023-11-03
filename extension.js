791808bfb0217ccb4b039fc622c500b49553b7d0
Shakil Ahmed <sakilhossain01969@gmail.com>
2023-11-03 20:58:22 +0600

    Update: extension command name updated

--- extension.js
+++ extension.js
@@ -53,7 +53,7 @@ function activate(context) {
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
-    "localizer.helloWorld",
+    "wsus.localizer",
     function () {
       // The code you place here will be executed every time your command is executed
 
